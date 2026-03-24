88496d6576edbe0ce4b8d5b9d15f321727533f0d
Yi Jiang <johnnyjiangyi01@gmail.com>
2026-03-24 20:43:47 +1100

    实现好友列表前端

--- backend/web/views/friend/get_list.py
+++ backend/web/views/friend/get_list.py
@@ -1,7 +1,3 @@
-from os import name
-
-from django.contrib.messages.api import success
-from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -13,33 +9,34 @@ class GetListFriendView(APIView):
     permission_classes = [IsAuthenticated]
     def get(self, request):
         try:
-            items_count = int(request.query_params.get('items_count' ,0))
-            friends_raw = Friend.objects.filter(me__user = request.user).order_by('-update_time')[items_count: items_count +20]
+            items_count = int(request.query_params.get('items_count', 0))
+            friends_raw = Friend.objects.filter(
+                me__user=request.user
+            ).order_by('-update_time')[items_count: items_count + 20]
             friends = []
             for friend in friends_raw:
-                character = friends.character
+                character = friend.character
                 author = character.author
                 friends.append({
-                    'id' : friend.id,
-                    'character' : {
+                    'id': friend.id,
+                    'character': {
                         'id': character.id,
                         'name': character.name,
-                        'profile' :character.profile,
-                        'photo' : character.photo.url,
+                        'profile': character.profile,
+                        'photo': character.photo.url,
                         'background_image': character.background_image.url,
-                        'author' : {
-                            'user_id' : author.user_id,
-                            'username' : author.user.username,
-                            'photo' :author.photo.url,
+                        'author': {
+                            'user_id': author.user_id,
+                            'username': author.user.username,
+                            'photo': author.photo.url,
                         }
                     }
                 })
-
-                return Response({
-                    'results' :' success',
-                    'friends' : friends,
-                })
+            return Response({
+                'result': 'success',
+                'friends': friends,
+            })
         except:
             return Response({
-                'result': '系统异常，请稍后重试。'
+                'result': '系统异常，请稍后重试'
             })
